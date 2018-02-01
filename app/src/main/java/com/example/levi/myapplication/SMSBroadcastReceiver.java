4d495782556583afd9eb0d3d2e59d0a741dfc4b0
blevi05 <bazsolevii@gmail.com>
2018-02-01 19:25:33 +0100

    Masodik Feltoltes

---
+++ app/src/main/java/com/example/levi/myapplication/SMSBroadcastReceiver.java
@@ -0,0 +1,42 @@
+package com.example.levi.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by Levi on 2018. 02. 01..
+ */
+
+public class SMSBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+            Bundle bundle = intent.getExtras();
+            SmsMessage[] tomb = null;
+            String sender = "";
+            String message = "";
+
+            if (bundle!=null){
+                Object[] pdus = (Object[]) bundle.get("pdus");
+                tomb = new SmsMessage[pdus.length];
+
+                for (int i = 0; i < pdus.length; i++) {
+                    tomb[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+
+                    if (i==0){
+                        sender = tomb[i].getOriginatingAddress();
+                    }
+                    message = message + tomb[i].getMessageBody();
+                }
+            }
+
+            Toast.makeText(context,"Küldő: " + sender + "Üzenet: " + message, Toast.LENGTH_LONG).show();
+
+        abortBroadcast();
+    }
+}
